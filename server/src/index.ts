c33cae886e7f379b7fcb67d97a3ea239e5be9d56
dahmane7 <bilelzineb@gmail.com>
2025-09-05 16:27:29 +0200

    Add files via upload

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,7 +15,7 @@ const app = express();
 const corsOptions = {
   origin: true, // Autorise toutes les origines (peut être remplacé par une liste d'origines)
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 } as const;
 
